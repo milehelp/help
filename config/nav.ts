56542df398d1675fd3749c8de6a95d33fe61b407
蓝莓君 <97638466@qq.com>
2025-09-26 16:29:06 +0800

    Upload nav.ts

---
+++ config/nav.ts
@@ -0,0 +1,18 @@
+export const nav = [
+  {
+    title: "下载软件",
+    link: "/download"
+  },
+  {
+    title: "帮助文档",
+    link: "/help"
+  },
+  {
+    title: "结绳社区",
+        link: "/bbs"
+  },
+  {
+    title: "联系我们",
+    link: "/company"
+  }
+]
